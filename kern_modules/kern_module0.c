c62e4762d68688e88902c41baeb987266dede3c8
Giorgi Mirianashvili <giorgimirianashvili27@gmail.com>
2024-07-23 20:50:16 +0400

    Update kern_module0.c

--- kern_modules/kern_module0.c
+++ kern_modules/kern_module0.c
@@ -1,12 +1,13 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 
-static int init_module(){
-	printk(KERN_ALERT "Loaded! Hello World! \n");
+static int init_module(){ // main function
+	printk(KERN_INFO "Edit text\n"); // use this for a simple kernel log
+	printk(KERN_ALERT "Edit text\n"); // use this for kernel alerts
 	return 0;
 }
-static void init_module_exit(void){
-	printk(KERN_ALERT "Exitted!\n");
+static void init_module_exit(void){ // exit function
+	printk(KERN_ALERT "Edit text\n"); // use this for kernel alert exit
 }
 
 module_init(init_module);
